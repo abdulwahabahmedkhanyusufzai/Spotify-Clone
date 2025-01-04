9c9e7dc3587847f04523fcbd8725ba4fa5ba8a08
abdulwahabahmedkhanyusufzai <absurw@gmail.com>
2025-01-04 14:16:59 +0500

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import {Routes,Route} from "react-router-dom"
+import HomePage from "./pages/home/HomePage"
+import AuthCallbackPage from "./pages/AuthCallback/AuthCallbackPage"
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
+import MainLayout from "./layout/Mainlayout"
+import ChatPage from "./pages/chat/ChatPage"
+import AlbumPage from "./pages/albums/AlbumPage"
+import AdminPage from "./pages/admin/AdminPage"
+function App() {
+  
+  return (
+   <>
+    <Routes>
+      
+      <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback
+      signUpForceRedirectUrl={"/auth-callback"}/>}/>
+      <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
+      <Route path='/admin' element={<AdminPage />} />
+      
+      <Route element={<MainLayout/>} >
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/chat" element={<ChatPage />} />
+      <Route path="/albums/:albumId" element={<AlbumPage/>}/>
+      </Route>
+    </Routes>
+   </>
+  )
+}
+
+export default App
